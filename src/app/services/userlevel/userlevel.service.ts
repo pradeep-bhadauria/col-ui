dc42b240fd82bfd57ded5cea18c4c2e5ac4c5666
Pradeep Bhadauria <pradeep.bhadauria08@gmail.com>
2018-05-01 17:42:11 +0530

    User level module update

--- src/app/services/userlevel/userlevel.service.ts
+++ src/app/services/userlevel/userlevel.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import { Constants, AlertService } from './../../utils/index';
+import { Constants, AlertService, AlertComponent } from './../../utils/index';
 import {UserLevel} from './../../models/index';
 import 'rxjs/Rx';
 
@@ -16,12 +16,44 @@ export class UserlevelService {
             );
     }
 
+    delete(userLevelId: number) {
+        return this.http.delete( 
+            Constants.API_ENDPOINT + '/user-level/' + userLevelId, this.jwt()).map(
+                (response: Response) => response.json()
+            );
+    }
+
     getAll(offset: number) {
         return this.http.get( 
             Constants.API_ENDPOINT + '/user-level/offset/' + offset, this.jwt()).map(
                 (response: Response) => response.json()
             );
     }
+
+    update(id: number, name: string, desc: string) {
+        let body = undefined;
+        body = {
+            user_level_name: name,
+            user_level_desc: desc
+        }
+        return this.http.put( 
+            Constants.API_ENDPOINT + '/user-level/' + id,body, this.jwt()).map(
+                (response: Response) => response.json()
+            );
+    }
+
+    add(name: string, desc: string) {
+        let body = undefined;
+        body = {
+            user_level_name: name,
+            user_level_desc: desc
+        }
+        return this.http.post( 
+            Constants.API_ENDPOINT + '/user-level/', body, this.jwt()).map(
+                (response: Response) => response.json()
+            );
+    }
+
 
     // private helper methods
     private jwt() {
--- src/app/userlevel/userlevel.component.ts
+++ src/app/userlevel/userlevel.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserlevelService } from './../services/index';
-import {UserLevel} from './../models/index';
+import { UserLevel } from './../models/index';
 import { Constants, AlertService } from './../utils/index';
+import { catchError } from 'rxjs/operators';
 
 
 @Component({
@@ -10,24 +11,26 @@ import { Constants, AlertService } from './../utils/index';
   styleUrls: ['./userlevel.component.css']
 })
 export class UserlevelComponent implements OnInit {
-  ngOnInit() {}
+  ngOnInit() { }
 
   loading = true;
   pageLimit = Constants.DEFAULT.TABLE_PAGINATION_LIMIT;
   offset = Constants.DEFAULT.OFFSET;
   pageOptions = Constants.DEFAULT.TABLE_PAGE_OPTIONS;
-  addUserLevel=false;
-  arrUserLevel = new Array(); 
+  addUserLevelContainer = false;
+  arrUserLevel = new Array();
+  isEditing=false;
+  pValName="";pValDesc="";
   constructor(private userLevelService: UserlevelService, private alertService: AlertService) {
     this.loading = true;
-    this.getAll(0);
-  } 
-  
-  getAll(offset: number){
-    if(offset==null){offset=0;}
+    this.getAll(this.offset);
+  }
+
+  getAll(offset: number) {
+    if (offset == null) { offset = 0; }
     this.userLevelService.getAll(offset).subscribe(
       data => {
-        if(data.data != undefined) {
+        if (data.data != undefined) {
           var ul = JSON.parse(data.data);
           ul.forEach(e => {
             var u = new UserLevel;
@@ -44,15 +47,20 @@ export class UserlevelComponent implements OnInit {
         this.loading = false;
       },
       error => {
-        this.alertService.error(error);
+        try{
+          this.alertService.error(JSON.parse(error._body).message);
+        }
+        catch {  
+          this.alertService.error("Server Error: Please try after some time.");
+        }
       }
     );
   }
 
-  getUserLevel(userLevelId: number){
+  getUserLevel(userLevelId: number) {
     this.userLevelService.getUserLevel(userLevelId).subscribe(
       data => {
-        if(data.data != undefined) {
+        if (data.data != undefined) {
           var ul = JSON.parse(data.data);
           this.alertService.success(data.message);
         } else {
@@ -61,30 +69,195 @@ export class UserlevelComponent implements OnInit {
         this.loading = false;
       },
       error => {
-        this.alertService.error(error);
+        try{
+          this.alertService.error(JSON.parse(error._body).message);
+        }
+        catch {  
+          this.alertService.error("Server Error: Please try after some time.");
+        }
       }
     );
   }
 
-  delete(){
-    //console.log(this);    
-  }
-
-  addUserLevelMain() {
-    if (this.addUserLevel== true){
-      this.addUserLevel=false;
-    } else {
-      this.addUserLevel=true;
+  delete(id,name) {
+    if(confirm("Are you sure to delete "+name)) {
+      this.userLevelService.delete(id).subscribe(
+        data => {
+          if (data.data != undefined) {
+            var ul = JSON.parse(data.data);
+            this.alertService.success(data.message);
+          } else {
+            this.alertService.error(data.message);
+          }
+          this.loading = false;
+          this.arrUserLevel = Array();
+          this.getAll(this.offset);
+        },
+        error => {
+          try{
+            this.alertService.error(JSON.parse(error._body).message);
+          }
+          catch {  
+            this.alertService.error("Server Error: Please try after some time.");
+          }
+        }
+      );
     }
   }
 
-  pageLimitChanged(value: number){
+  addUserLevelMain() {
+    if (this.addUserLevelContainer == true) {
+      this.addUserLevelContainer = false;
+    } else {
+      this.addUserLevelContainer = true;
+    }
+  }
+
+  addUserLevel(name, desc) {
+    this.userLevelService.add(name.value.toString(), desc.value.toString()).subscribe(
+      data => {
+        if (data.data != undefined) {
+          var ul = JSON.parse(data.data);
+          this.alertService.success(data.message);
+        } else {
+          this.alertService.error(data.message);
+        }
+        this.loading = false;
+        this.arrUserLevel = Array();
+        this.getAll(0);
+      },
+      error => {
+        try{
+          this.alertService.error(JSON.parse(error._body).message);
+        }
+        catch {  
+          this.alertService.error("Error: Pls check the values. Name must be unique.");
+        }
+      }
+    );
+  }
+
+  pageLimitChanged(value: number) {
     this.pageLimit = value;
   }
 
-  add(a:number,b:number){
-    var r = a+b;
+  add(a: number, b: number) {
+    var r = a + b;
     return parseInt(r.toString());
   }
+
+  save(id, e){
+    var target = e.currentTarget;
+    var tr = target.parentElement.parentElement;
+    var name = tr.children[0].innerHTML.toString().trim();
+    var desc = tr.children[1].innerHTML.toString().trim();
+
+    if(name != "" && desc != ""){
+      this.userLevelService.update(id, name, desc).subscribe(
+        data => {
+          console.log(data);
+          if (data.data != undefined) {
+            var ul = JSON.parse(data.data);
+            this.alertService.success(data.message);
+          } else {
+            this.alertService.error(data.message);
+          }
+          this.loading = false;
+          this.isEditing=false;
+          var pElement = target.parentElement;
+          var edit = pElement.children[0];
+          var del = pElement.children[1];
+          var save = pElement.children[2];
+          var cancel = pElement.children[3];
+          save.classList.remove("visible");
+          save.classList.add("hidden");
+          cancel.classList.remove("visible");
+          cancel.classList.add("hidden");
+          edit.classList.remove("hidden");
+          edit.classList.add("visible");
+          del.classList.remove("hidden");
+          del.classList.add("visible");
+          var tr = target.parentElement.parentElement;
+          tr.classList.remove("editing")
+          tr.children[0].classList.remove("editable")
+          tr.children[1].classList.remove("editable")
+          tr.children[0].contentEditable=false;
+          tr.children[1].contentEditable=false;
+        },
+        error => {
+          try{
+            this.alertService.error(JSON.parse(error._body).message);
+          }
+          catch {  
+            this.alertService.error("Error: Pls check the values. Name must be unique.");
+          }
+        }
+      );
+    } else {
+      this.alertService.error('Value should be atleast 3 characters long.');
+    }
+  }
+
+  edit(e) {
+    if(this.isEditing==false){
+      this.isEditing=true;
+      var target = e.currentTarget;
+      var pElement = target.parentElement;
+      var edit = pElement.children[0];
+      var del = pElement.children[1];
+      var save = pElement.children[2];
+      var cancel = pElement.children[3];
+      save.classList.remove("hidden");
+      save.classList.add("visible");
+      cancel.classList.remove("hidden");
+      cancel.classList.add("visible");
+      edit.classList.remove("visible");
+      edit.classList.add("hidden");
+      del.classList.remove("visible");
+      del.classList.add("hidden");
+
+      var tr = target.parentElement.parentElement;
+      tr.classList.add("editing");
+      tr.children[0].classList.add("editable")
+      tr.children[1].classList.add("editable")
+      tr.children[0].contentEditable=true;
+      tr.children[1].contentEditable=true;
+      tr.children[0].focus();
+      this.pValName=tr.children[0].innerHTML.toString().trim();
+      this.pValDesc = tr.children[1].innerHTML.toString().trim();
+    }
+    else {
+      this.alertService.error('Pls save/cancel changes before editing next.');
+    }
+    
+
+  }
+
+  cancelEdit(e) {
+    this.isEditing=false;
+    var target = e.currentTarget;
+    var pElement = target.parentElement;
+    var edit = pElement.children[0];
+    var del = pElement.children[1];
+    var save = pElement.children[2];
+    var cancel = pElement.children[3];
+    save.classList.remove("visible");
+    save.classList.add("hidden");
+    cancel.classList.remove("visible");
+    cancel.classList.add("hidden");
+    edit.classList.remove("hidden");
+    edit.classList.add("visible");
+    del.classList.remove("hidden");
+    del.classList.add("visible");
+    var tr = target.parentElement.parentElement;
+    tr.classList.remove("editing")
+    tr.children[0].classList.remove("editable")
+    tr.children[1].classList.remove("editable")
+    tr.children[0].contentEditable=false;
+    tr.children[1].contentEditable=false;
+    tr.children[0].innerHTML=this.pValName;this.pValName="";
+    tr.children[1].innerHTML=this.pValDesc;this.pValDesc="";
+  }
+  
 
 }
