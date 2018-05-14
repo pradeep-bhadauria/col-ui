4f1a167e81b60666f4b28abdaf8738b5c4820eb5
Pradeep Bhadauria <pradeep.bhadauria08@gmail.com>
2018-05-14 16:23:27 +0530

    updated profile setting

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProfileService } from './../services/index';
+import { ProfileService, UserService } from './../services/index';
 import { Constants, AlertService } from './../utils/index';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  user = JSON.parse(localStorage.getItem('currentUser'));
+  user = null;
   active = "myarticles";
 
   articleCount = 0;
@@ -17,10 +17,27 @@ export class ProfileComponent implements OnInit {
   offset = 0;
   limit = Constants.DEFAULT.TABLE_PAGINATION_LIMIT;
 
+  fname="";
+  fname_err="";
+  lname="";
+  lname_err="";
+  password="";
+  password_err="";
+  confirm="";
+  confirm_err="";
+  email="";
+  email_err="";
+
   constructor(
     private profileService: ProfileService,
     public alertService: AlertService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+    if(this.user == null){
+      window.location.href = "/login"
+    }
   }
   ngOnInit(){
     this.route.params.subscribe(params => {
@@ -51,6 +68,115 @@ export class ProfileComponent implements OnInit {
         }
       }
     );
+  }
+
+  changeEmail(){
+    if(this.validate("email")){
+      this.userService.updateEmail(this.user.id, this.email).subscribe(
+        data => {
+          this.alertService.success(data.message);
+        },
+        error=>{
+          try{
+            this.alertService.error(JSON.parse(error._body).message);
+          }
+          catch {  
+            this.alertService.error("Server Error: Please try after some time.");
+          }
+        }
+      );
+    }
+  }
+
+  changePassword(){
+    if(this.validate("password")){
+      this.userService.updatePassword(this.user.id, this.password).subscribe(
+        data => {
+          this.alertService.success(data.message);
+        },
+        error=>{
+          try{
+            this.alertService.error(JSON.parse(error._body).message);
+          }
+          catch {  
+            this.alertService.error("Server Error: Please try after some time.");
+          }
+        }
+      );
+    }
+  }
+
+  changeName(){
+    if(this.validate("name")){
+      this.userService.updateName(this.user.id, this.fname, this.lname).subscribe(
+        data => {
+          this.alertService.success(data.message + " We are logging you out for name to reflect.");
+          setTimeout(function(){
+            localStorage.removeItem("currentUser");
+            window.location.href = "/login";
+          },5000);
+        },
+        error=>{
+          try{
+            this.alertService.error(JSON.parse(error._body).message);
+          }
+          catch {  
+            this.alertService.error("Server Error: Please try after some time.");
+          }
+        }
+      );
+    }
+  }
+
+  validate(name: string){
+    var allGood = true;
+    if(name=="name"){
+      if (this.fname.length < 3) {
+        this.fname_err = "Should be atleast 3 characters"; allGood = false;
+      } else {
+        this.fname_err = ""
+      }
+      if (this.lname.length < 3) {
+        this.lname_err = "Should be atleast 3 characters"; allGood = false;
+      } else {
+        this.lname_err = ""
+      }
+    } else if (name=="email"){
+      if(this.email == "" || !this.isValidEmail(this.email)){
+        this.email_err = "Email address not valid"; allGood = false;
+      } else {
+        this.email_err = ""
+      }
+    } else if (name=="password"){
+      if (this.password.length < 6 || this.password.length > 30) {
+        this.password_err = "Should be between 6 - 30 characters"; allGood = false;
+      } 
+      else if(!this.isValidPassword(this.password)){
+        this.password_err = "Can contain A-Z a-z 0-9 _ - ! @ # $ % & * ( )"; allGood = false;
+      }
+      else {
+        this.password_err = ""
+      }
+      
+      if (this.confirm == "") { this.confirm_err = "Field is required."; allGood = false; } else { this.confirm_err = "" }
+
+      if (this.password != this.confirm) {
+        this.confirm_err = "Password doesn't match";
+        allGood = false;
+      } else {
+        this.confirm_err = ""
+      }
+    }
+    return allGood;
+  }
+
+  public isValidEmail(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return re.test(String(email).toLowerCase());
+  }
+  public isValidPassword(p) {
+    var re = /^[-A-Za-z0-9_!@#$%&*()]*$/
+    return re.test(String(p));
   }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -47,6 +47,84 @@ export class UserService {
       );
   }
 
+  updateEmail(user_id: string, email: string){
+    let body = undefined;
+    body = {
+      user_email: email
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/email', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateName(user_id: string, user_fname: string, user_lname: string){
+    let body = undefined;
+    body = {
+      user_fname: user_fname,
+      user_lname: user_lname
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/name', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateDob(user_id: string, dob: string){
+    let body = undefined;
+    body = {
+      dob: dob
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/dob', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateDoe(user_id: string, doe: string){
+    let body = undefined;
+    body = {
+      doe: doe
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/doe', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateIsActive(user_id: string, is_active: string){
+    let body = undefined;
+    body = {
+      is_active: is_active
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/active', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateIsBlocked(user_id: string, is_blocked: string){
+    let body = undefined;
+    body = {
+      is_blocked: is_blocked
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/blocked', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
+  updateUserLevel(user_id: string, user_level: string){
+    let body = undefined;
+    body = {
+      user_level: user_level
+    }
+    return this.http.put(
+      Constants.API_ENDPOINT + '/users/'+ user_id +'/level', body, Constants.jwt()).map(
+        (response: Response) => response.json()
+      );
+  }
+
   sendChangePasswordLink(email: String) {
     let body = undefined;
     body = {
